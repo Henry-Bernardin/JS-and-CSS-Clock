746c6ac5e6cb1052df84cedcc7447d3d182bb076
Henry-Bernardin <henry.bernardin@gmail.com>
2019-12-06 09:00:26 -0500

    Logic for minute hand

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const secondHand = document.querySelector('.second-hand'); 
 const minHand = document.querySelector('.min-hand');
 
+
 function setDate() {
  const now = new Date(); 
  const seconds = now.getSeconds(); 
+ const minutes = now.getMinutes();
  const secondDegrees = ((seconds / 60) * 360) + 90; 
+ const minuteDegrees = ((minutes/60) * 360) + 90;
  secondHand.style.transform = `rotate(${secondDegrees}deg)`; 
+ minHand.style.transform = `rotate(${minuteDegrees}deg)`; 
+
 }
 
 setInterval(setDate, 1000); 
